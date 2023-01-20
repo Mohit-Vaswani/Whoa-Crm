2528db1ef85f2fa72d710577d99ba030bbf0f638
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-01-20 22:02:22 +0530

    Messages added

--- public/js/message.js
+++ public/js/message.js
@@ -1,19 +1,19 @@
-const demoText = document.getElementById("demoText"); // demoText
-const inputMsg = document.getElementById("inputMsg"); // InputMsg
-const msgSubmit = document.getElementById("msgSubmit"); // MessageBtn
+// const demoText = document.getElementById("demoText"); // demoText
+// const inputMsg = document.getElementById("inputMsg"); // InputMsg
+// const msgSubmit = document.getElementById("msgSubmit"); // MessageBtn
 
-msgSubmit.addEventListener("click", function() {
-    demoText.classList.add("p-2")
-    const message = inputMsg.value;
-    demoText.innerHTML += message + "<br>";
-    inputMsg.value = "";
-});
+// msgSubmit.addEventListener("click", function() {
+//     demoText.classList.add("p-2")
+//     const message = inputMsg.value;
+//     demoText.innerHTML += message + "<br>";
+//     inputMsg.value = "";
+// });
 
-inputMsg.addEventListener("keypress", function(e) {
-    if (e.key === "Enter") {
-      demoText.classList.add("p-2")
-    const message = inputMsg.value;
-    demoText.innerHTML += message + "<br>";
-    inputMsg.value = "";
-  }
-});
+// inputMsg.addEventListener("keypress", function(e) {
+//     if (e.key === "Enter") {
+//       demoText.classList.add("p-2")
+//     const message = inputMsg.value;
+//     demoText.innerHTML += message + "<br>";
+//     inputMsg.value = "";
+//   }
+// });
