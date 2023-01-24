6961be354ea0ec88bd931726ab1616cb11eb48f0
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-01-24 14:36:08 +0530

    some minor changes.

--- public/whoa1.js
+++ public/whoa1.js
@@ -6,3 +6,13 @@ const dotBtn = document.getElementById("dotBtn");
 threeDot.addEventListener('click', ()=>{
   dotBtn.classList.remove('hidden')
 })
+
+
+// Three dot button
+
+const deleteDot = document.getElementById("deleteDot");
+const deleteBtn = document.getElementById("deleteBtn");
+
+deleteDot.addEventListener('click', ()=>{
+  deleteBtn.classList.remove('hidden')
+})
